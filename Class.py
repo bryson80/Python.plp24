9c31e397f37971df35a608f5f813111fbb1cb56b
Bry_son <brayson@Braysons-MacBook-Pro.local>
2024-04-06 16:24:32 +0300

    Class.py

---
+++ Class.py
@@ -0,0 +1,14 @@
+class Person:
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+        
+        def introduce(self):
+            print("Hello, my name is {self.name}. I am {self.age} years old and I am {self.gender}.")
+            
+            # creating an instance of the person class
+            person1 = Person("Bryson", 34, "Man")
+            
+            # calling the introduce method to display the person's information
+            person1.introduce()
